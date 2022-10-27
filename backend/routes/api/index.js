a735813e86a5ed8fdeafebf93a99dc908b8b8c91
Alex <ajtravis2@gmail.com>
2022-10-27 18:05:02 -0400

    added spots router and route handler to get all spots

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
+const spotsRouter = require('./spots.js')
 const { requireAuth } = require('../../utils/auth.js');
 
 const { restoreUser } = require("../../utils/auth.js");
@@ -24,6 +25,8 @@ router.use(restoreUser);
 router.use('/session', sessionRouter);
 
 router.use('/users', usersRouter);
+
+router.use('/spots', spotsRouter)
 
 
 
---
+++ backend/routes/api/spots.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { User, Spot } = require('../../db/models');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    const spots = await Spot.findAll()
+    return res.json(spots)
+})
+
+module.exports = router;
