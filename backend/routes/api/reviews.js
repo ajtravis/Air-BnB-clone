e231b6dd54f25821d1f02227c1468807c34ef097
Alex <ajtravis2@gmail.com>
2022-10-28 18:14:47 -0400

    created review router and route handler to get reviews for current user

---
+++ backend/routes/api/reviews.js
@@ -0,0 +1,19 @@
+const express = require('express')
+
+const { setTokenCookie, requireAuth, restoreUser } = require('../../utils/auth');
+const { User, Spot, SpotImage, Review } = require('../../db/models');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+const { Op } = require("sequelize");
+
+const router = express.Router();
+
+router.get('/current', async (req, res) => {
+    const { user } = req;
+    const reviews = await Review.findAll({
+        where: {userId: user.id}
+    });
+    res.json(reviews)
+})
+
+module.exports = router;
