75552f8afce853bbf377f88134a8c330b00b16d5
Alex <ajtravis2@gmail.com>
2022-10-28 12:20:26 -0400

    added endpoint to get spots for the current user

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,19 +1,31 @@
 const express = require('express')
 
-const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { setTokenCookie, requireAuth, restoreUser } = require('../../utils/auth');
 const { User, Spot } = require('../../db/models');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
+const { Op } = require("sequelize");
 
 const router = express.Router();
 
+// get all spots
 router.get('/', async (req, res) => {
     const spots = await Spot.findAll()
     return res.json(spots)
 });
 
+// get spots for current user
 router.get('/current', async (req, res) => {
-    
-})
+    restoreUser;
+    const { user } = req;
+    const spots = await Spot.findAll({
+        where: {
+            ownerId: user.id
+        }
+    })
+    return res.json(spots)
+});
+
+
 
 module.exports = router;
