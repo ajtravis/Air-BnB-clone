fe159d331942a12ab95f629c24d42c232287ece8
Alex <ajtravis2@gmail.com>
2022-10-29 17:40:33 -0400

    added endpoint to delete a spot image

--- backend/routes/api/spot-images.js
+++ backend/routes/api/spot-images.js
@@ -1,13 +1,22 @@
 const express = require('express')
 
 
-const { User, Spot, SpotImage, Review, ReviewImage, SpotImage } = require('../../db/models');
+const { User, Spot, SpotImage, Review, ReviewImage} = require('../../db/models');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 const { Op } = require("sequelize");
 
 const router = express.Router();
 
-
+router.delete('/:imageId', async (req, res) => {
+    const split = req.url.split('/')
+    const imageId = split[split.length - 1];
+    const spotImage = await SpotImage.findByPk(imageId);
+    await spotImage.destroy();
+    return res.json({
+        "message": "Successfully deleted",
+        "statusCode": 200
+      })
+})
 
 module.exports = router;
