72a71d87289086412447a941920c0f45d2e2a720
Alex <ajtravis2@gmail.com>
2022-12-13 11:44:15 -0500

    AddSpot folder created in components

---
+++ frontend/src/components/AddSpotForm/index.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addSpot } from '../../store/spots'
+
+const AddSpotForm = () => {
+    return (
+        <form>
+
+        </form>
+    )
+}
+
+export default AddSpotForm;
