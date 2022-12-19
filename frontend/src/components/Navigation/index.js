fa850333a0970144ee98c741670197439d9e837c
Alex <ajtravis2@gmail.com>
2022-12-18 22:41:26 -0500

    added logo, hover effects for menu

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -3,12 +3,16 @@ import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
+import logo from "./airbnb-2.png"
 
 function Navigation({ isLoaded }){
   const sessionUser = useSelector(state => state.session.user);
 
   return (
     <div className="navBar">
+      <NavLink exact to={"/"}>
+      <img className='logo' src={logo} alt="logo.png"/>
+      </NavLink>
       <ul className='menu'>
         <li>
           <NavLink exact to="/">Home</NavLink>
