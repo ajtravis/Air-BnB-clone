be2006c3f65a7070654da99cfcaacecd60eb5f53
Alex <ajtravis2@gmail.com>
2023-01-18 09:33:21 -0500

    style spot cards complete

--- frontend/src/components/SpotCard/index.js
+++ frontend/src/components/SpotCard/index.js
@@ -27,13 +27,18 @@ const SpotCard = ({id, city, state, avgRating, price, previewImage}) => {
             <div className="img-container">
             <img src={previewImage} alt="img.jpg" />
             </div>
-            <div className="card-info">
-                <div className="area">{city}, {state}</div>
-                <div className="rating">
-                <i className="fa-sharp fa-solid fa-star"></i>
-                {Math.round(avgRating * 100)/100} </div>
+            <div className="info-container">
+                <div className="card-info">
+                    <div className="area">{city}, {state}</div>
+                    <div className="rating">
+                        <i className="fa-sharp fa-solid fa-star"></i>
+                    {Math.round(avgRating * 100)/100} </div>
+                </div>
+            <div className="price-container">
+                <h4 className="price"> ${Math.round(price)}</h4>
+                <h5 className="night">night</h5>
             </div>
-            <div className="price"> ${Math.round(price)} </div>
+            </div>
         </div>
     )
 }
